59be3941364481196a6f5fe711725148fb6e6526
Calvin <calvinzheng0119@gmail.com>
2018-11-19 14:44:16 -0500

    Add .gitignore file.

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+public class Test
+{
+    public static void main (String[] args)
+    {
+        int[] arr1 = { 923, 516, 669, 7852, 964, 6354, 9643, 219, 785, 102};
+        Sorts.printArr(arr1);
+        System.out.println();
+
+        Sorts.ascendingSort(arr1);
+        Sorts.printArr(arr1);
+        System.out.println();
+
+    }
+}
