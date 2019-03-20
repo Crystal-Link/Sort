b534dfd4391755971066766b779d22e3b892a8c1
Calvin <calvinzheng0119@gmail.com>
2019-03-20 14:45:57 -0400

    adds mergeSort

--- src/Sorts.java
+++ src/Sorts.java
@@ -1,33 +1,25 @@
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.math.*;
 
 public class Sorts
 {
-    public static void ascendingSort(int [] arr)
+    public static void ascendingSort(int[] arr)
     {
-        for (int i = 0; i < arr.length-1; i++)
+        for (int i = 0; i < arr.length - 1; i++)
         {
             int lowPosition = i;
 
-            for (int j = i+1; j < arr.length; j++)
+            for (int j = i + 1; j < arr.length; j++)
             {
-                if(arr[j] < arr[lowPosition])
+                if (arr[j] < arr[lowPosition])
+                {
                     lowPosition = j;
+                }
+
+                swap(i, lowPosition, arr);
             }
-            swap(i, lowPosition, arr);
-        }
-    }
-    public static void ascendingSort(List<String> list)
-    {
-        for (int i = 0; i < list.size()-1; i++)
-        {
-            int lowPosition = i;
-            for (int j = i+1; j < list.size(); j++)
-            {
-                if(list.get(j).compareTo(list.get(lowPosition)) < 0)
-                    lowPosition = j;
-            }
-            list.set(i, list.set(lowPosition, list.get(i)));
         }
     }
 
@@ -38,35 +30,152 @@ public class Sorts
         arr[indexB] = temp;
     }
 
-    public static void printArr(int[] arr)
+    public static void printArray(int[] arr)
     {
-        for (int num: arr)
-            System.out.print(num + " ");
+        for (int n : arr)
+        {
+            System.out.print(n + ", ");
+        }
         System.out.println();
     }
 
-    public static void printStringArr(String[] arr)
+    public static void printArray(List<String> list)
     {
-        for (String str: arr)
-            System.out.print(str + " ");
+        for (String n : list)
+        {
+            System.out.print(n + ", ");
+        }
         System.out.println();
     }
 
-    public static List<String> removeLongerThan (int maxLen, List<String> list)
+    public static void ascendingSort(List<String> list)
     {
-       /* for(int i = list.size()-1; i >= 0; i--)
-            if (list.get(i).length() > maxLen)
-                list.remove(i); */
-       ArrayList<String> removedNames = new ArrayList<String>();
-       int i = 0;
-       while (i < list.size())
-       {
-           if (list.get(i).length() > maxLen)
-           {
-                removedNames.add(list.remove(i));
-           }
-           else i++;
-       }
-       return removedNames;
+        for (int i = 0; i < list.size() - 1; i++) {
+            int lowPosition = i;
+            for (int h = i + 1; h < list.size(); h++)
+            {
+                if (list.get(h).compareTo(list.get(lowPosition)) < 0)
+                    lowPosition = h;
+            }
+            list.set(i, list.set(lowPosition, list.get(i)));
+        }
+    }
+
+    public static ArrayList<String> RemoveLongerThan(int lenMax, List<String> list)
+    {
+        ArrayList<String> removedList = new ArrayList<String>();
+        for (int i = list.size() - 1; i >= 0; i--)
+        {
+            if (list.get(i).length() > lenMax)
+            {
+                removedList.add(list.remove(i));
+            }
+        }
+
+        return removedList;
+    }
+    public static void selectionSort(int[] elements)
+    {
+        for (int j = 0; j < elements.length - 1; j++)
+        {
+            int minIndex = j;
+            for (int k = j + 1; k < elements.length; k++)
+            {
+                if (elements[k] < elements[minIndex])
+                {
+                    minIndex = k;
+                }
+            }
+            int temp = elements[j];
+            elements[j] = elements[minIndex];
+            elements[minIndex] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] elements)
+    {
+        for (int j = 1; j < elements.length; j++)
+        {
+            int temp = elements[j];
+            int possibleIndex = j;
+            while (possibleIndex > 0 && temp < elements[possibleIndex - 1])
+            {
+                elements[possibleIndex] = elements[possibleIndex - 1];
+                possibleIndex--;
+            }
+            elements[possibleIndex] = temp;
+        }
+    }
+
+    public static void mergeSort(int[] elements)
+    {
+        int n = elements.length;
+        int[] temp = new int[n];
+        mergeSortHelper(elements, 0, n-1, temp);
+    }
+
+    private static void mergeSortHelper(int[] elements,
+                                        int from, int to, int[] temp)
+    {
+        if (from < to)
+        {
+            int middle = (from + to) / 2;
+            mergeSortHelper(elements, from, middle, temp);
+            mergeSortHelper(elements, middle + 1, to, temp);
+            merge(elements, from, middle, to, temp);
+        }
+    }
+
+    private static void merge(int[] elements,
+                              int from, int mid, int to, int[] temp)
+    {
+        int i = from;
+        int j = mid + 1;
+        int k = from;
+        while (i <= mid && j <= to)
+        {
+            if (elements[i] < elements[j])
+            {
+                temp[k] = elements[i];
+                i++;
+            }
+            else
+            {
+                temp[k] = elements[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= mid)
+        {
+            temp[k] = elements[i];
+            i++;
+            k++;
+        }
+        while (j <= to)
+        {
+            temp[k] = elements[j];
+            j++;
+            k++;
+        }
+        for (k = from; k <= to; k++)
+        {
+            elements[k] = temp[k];
+        }
+    }
+
+    public static int getRandomNumber (int maxPower)
+    {
+        return (int)(Math.random() * Math.pow(10, maxPower));
+    }
+
+    public static int[] buildArray(int length)
+    {
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++)
+        {
+            arr[i] = getRandomNumber(6);
+        }
+        return arr;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,36 @@ public class Test
 {
     public static void main (String[] args)
     {
+        int[] arr1 = Sorts.buildArray(100000);
+        int[] arr2 = arr1.clone();
+
+        long start1 = System.currentTimeMillis();
+        Sorts.selectionSort(arr1);
+        long end1 = System.currentTimeMillis();
+        long selectionTime = end1 - start1;
+        System.out.println("Selection sort on unsorted data: " + selectionTime);
+
+        long start1a = System.currentTimeMillis();
+        Sorts.selectionSort(arr1);
+        long end1a = System.currentTimeMillis();
+        long selectionTime1a = end1a - start1a;
+        System.out.println("Selection sort on sorted data: " + selectionTime1a);
+
+
+        long start2 = System.currentTimeMillis();
+        Sorts.insertionSort(arr2);
+        long end2 = System.currentTimeMillis();
+        long insertionTime = end2 - start2;
+        System.out.println("Insertion sort on unsorted data: " + insertionTime);
+
+
+        long start2a = System.currentTimeMillis();
+        Sorts.insertionSort(arr2);
+        long end2a = System.currentTimeMillis();
+        long insertionTimea = end2a - start2a;
+        System.out.println("Insertion sort on sorted data: " + insertionTimea);
+
+
         int[] arr1 = { 923, 516, 669, 7852, 964, 6354, 9643, 219, 785, 102};
         Sorts.printArr(arr1);
         System.out.println();
